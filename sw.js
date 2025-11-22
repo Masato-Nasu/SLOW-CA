2b4390f35f3ae570254cdb5e798d753a704da46f
那須　雅人 <1bitexist@gmail.com>
2025-11-22 23:37:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Lemma-Driven CA PWA Service Worker
+const CACHE_NAME = "lemma-ca-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME) ? caches.delete(k) : null))
+    ).then(self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
